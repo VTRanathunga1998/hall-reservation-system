2bdd4888ae14f172a40edea2ef83546203ea6985
Viraj Tharuka Ranathunga <viraj@jwareautomation.com>
2025-09-30 16:44:51 +0530

    basic frontend finished

--- components/FormModal.tsx
+++ components/FormModal.tsx
@@ -1,4 +1,23 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useState } from "react";
+import { ReactNode } from "react";
+
+const LecturerForm = dynamic(() => import("./forms/LecturerForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => ReactNode;
+} = {
+  lecturer: (type, data) => <LecturerForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 
 const FormModal = ({
   table,
@@ -18,12 +37,48 @@ const FormModal = ({
       : type === "update"
       ? "bg-[#C3EBFA]"
       : "bg-[#CFCEFF]";
+
+  const [open, setOpen] = useState(false);
+
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="p-4 flex flex-col gap-4">
+        <span className="text-center font-medium">
+          All data will be lost. Are you sure you want to delete this {table}?
+        </span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+          Delete
+        </button>
+      </form>
+    ) : type === "create" || type === "update" ? (
+      forms[table](type, data)
+    ) : (
+      "Form not found!"
+    );
+  };
+
   return (
-    <button
-      className={`${size} flex items-center justify-center rounded-full ${bgColor} cursor-pointer`}
-    >
-      <Image src={`/${type}.png`} alt="" height={16} width={16} />
-    </button>
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor} cursor-pointer`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" height={16} width={16} />
+      </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black/60 z-[150] flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
