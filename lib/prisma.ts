29f309b121a99a99999dbd47508fc44c54e70901
VTranathunga98 <virajtharuka.fb@gmail.com>
2025-12-11 20:00:37 +0530

    prisma connection updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
@@ -8,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    log: process.env.NODE_ENV === "production" ? ["error"] : ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
