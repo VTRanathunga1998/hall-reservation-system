c0b65d13acfbf9bbdeb963f27ec33d0d19a5ede2
VTranathunga98 <virajtharuka.fb@gmail.com>
2026-04-23 17:21:09 +0530

    changes in manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#000000" />
+        </head>
         <body className={inter.className}>
           {children} <ToastContainer position="bottom-right" theme="dark" />
         </body>
